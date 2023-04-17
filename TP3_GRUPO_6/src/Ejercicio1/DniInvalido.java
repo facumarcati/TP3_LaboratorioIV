dd9935b76425158090323c02e7dd022b93ee1111
Facundo Marcati <111024819+facumarcati@users.noreply.github.com>
2023-04-17 20:36:11 -0300

    Add files via upload
    Excepción de tipo IOException llamada DniInvalido

---
+++ TP3_GRUPO_6/src/Ejercicio1/DniInvalido.java
@@ -0,0 +1,18 @@
+package Ejercicio1;
+
+import java.io.IOException;
+
+public class DniInvalido extends IOException{
+
+	public DniInvalido() {
+		
+	}
+
+	@Override
+	public String getMessage() {
+		
+		return "DNI invalido";
+	}
+
+	
+}
